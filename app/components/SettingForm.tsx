e3f449a9e2434adfb010c4aa88ec03a2ced1705b
Falasefemi2 <femifalase228@gmail.com>
2024-04-19 21:33:08 +0100

    Settings page done

---
+++ app/components/SettingForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const SettingsForm = () => {
+    return (
+        <form>
+            <h1 className="text-3xl font-extralight tracking-tight">Settings</h1>
+        </form>
+    );
+}
+
+export default SettingsForm;
